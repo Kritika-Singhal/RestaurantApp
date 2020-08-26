37a4ae14e72a35c015a10b051780a8f553dd9a29
Kritika-Singhal <singhalk30@gmail.com>
2020-08-26 16:19:17 +0530

    Single Page Application Part2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,6 +23,14 @@ class MainComponent extends Component {
     };
 }
   render() {
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     const HomePage = () => {
       return (
         <Home
@@ -39,6 +47,7 @@ class MainComponent extends Component {
         <Switch>
           <Route path = "/home" component = {HomePage} /> 
           <Route exact path="/menu" component = {() => <Menu dishes={this.state.dishes}/>} />
+          <Route path='/menu/:dishId' component={DishWithId}/>
           <Route exact path='/contactus' component={Contact} />
           <Redirect to = '/home'/>
         </Switch> 
