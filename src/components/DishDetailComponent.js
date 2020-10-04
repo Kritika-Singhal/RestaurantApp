b794eb1b326fb9fdd572121e40dfa923f7840260
Kritika-Singhal <singhalk30@gmail.com>
2020-10-05 00:11:10 +0530

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -17,6 +17,7 @@ import {
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -55,7 +56,7 @@ function RenderComments({ comments, addComment, dishId }) {
       <div>
         <h4>Comments</h4>
         {commentlist}
-        <CommentForm addComment={addComment} dishId={dishId}/> 
+        <CommentForm addComment={addComment} dishId={dishId} />
       </div>
     );
   } else {
@@ -64,7 +65,23 @@ function RenderComments({ comments, addComment, dishId }) {
 }
 
 const DishDetail = (props) => {
-  if (props.dish != null)
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null)
     return (
       <div className="container">
         <div className="row">
@@ -84,9 +101,11 @@ const DishDetail = (props) => {
             <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments}
+            <RenderComments
+              comments={props.comments}
               addComment={props.addComment}
-              dishId={props.dish.id}/>
+              dishId={props.dish.id}
+            />
           </div>
         </div>
       </div>
@@ -114,7 +133,12 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.addComment(
+      this.props.dishId,
+      values.rating,
+      values.author,
+      values.comment
+    );
   }
 
   render() {
